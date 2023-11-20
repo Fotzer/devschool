b7993b859d73bd12fad61ea9cee04385080b6b9e
Maxik <mpoplavskiy38380@gmail.com>
2023-11-20 18:40:27 +0200

    Implement POST on /products

--- hw 7/server.js
+++ hw 7/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import customerController from "./Presentational layer/customerController.js";
 import employeeController from "./Presentational layer/employeeController.js";
 import orderController from "./Presentational layer/orderController.js";
+import productController from "./Presentational layer/productController.js";
 import bodyParser from "body-parser";
 
 const app = express();
@@ -13,5 +14,7 @@ app.get('/customers/:customerId/orders', customerController.getCustomerOrders);
 app.patch('/employees/:employeeId', employeeController.patchEmployee);
 
 app.delete('/orders/:orderId', orderController.deleteOrder);
+
+app.post('/products', productController.postProduct);
 
 app.listen(3000);
